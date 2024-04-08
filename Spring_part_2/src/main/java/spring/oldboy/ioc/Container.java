b99214ce1dd6e7fc044bfaac49771072c44c080a
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-08 23:11:03 +0500

    Add Container.java

---
+++ Spring_part_2/src/main/java/spring/oldboy/ioc/Container.java
@@ -0,0 +1,14 @@
+package spring.oldboy.ioc;
+
+/* Более наглядную реализацию принципа см. IoCDemo.java и IoCClasses */
+
+public class Container {
+
+    public <T> T get(Class<T> clazz) {
+        /*
+        Возвращаем из контейнера нужный объект
+        (естественно он там должен быть)
+        */
+        return null;
+    }
+}
