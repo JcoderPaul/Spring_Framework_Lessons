bd0bc79ce7b6709860c2ffae7d4cdfdbe90fee28
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-09 12:58:12 +0500

    Add StockRepository.java

---
+++ Spring_part_2/src/main/java/spring/oldboy/repository/StockRepository.java
@@ -0,0 +1,44 @@
+package spring.oldboy.repository;
+
+/* Применение аннотации @Autowired над внедряемым полем */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import spring.oldboy.pool.StarterConnectionPool;
+
+public class StockRepository{
+    /*
+    Аннотация @Autowired в Spring Framework используется для автоматического
+    связывания компонентов bean-a между собой. Она позволяет автоматически
+    настраивать свойства bean-a и методы, упрощая тем самым процесс инъекции
+    зависимостей.
+
+    В нашем application.xml, как минимум два однотипных bean-a:
+
+    <bean id="pool2" name="poolchic" class="spring.oldboy.pool.StarterConnectionPool"/>
+    <bean id="poolStarter" name="poolGate" class="spring.oldboy.pool.StarterConnectionPool"/>
+
+    Чтобы решить проблему неопределенности при выборе bean-a, можно использовать
+    аннотацию @Qualifier. Она позволяет указать, какой bean следует использовать
+    при внедрении. В этом примере, Spring внедрит бин с именем «pool2».
+
+    То что мы пытались сделать аннотацией @InjectBean делают:
+    - аннотация @Resource(name = "poolStarter") из пакета javax.annotation;
+    - @Autowired и @Qualifier("poolStarter") из org.springframework.beans.factory.annotation
+    */
+
+    /*
+    Во всех случаях, если у нас в Spring контейнере хранятся более одного однородного
+    bean-a, рекомендуется их именовать и явно прописывать имя используемого для внедрения,
+    например: @Resource(name = "poolStarter")
+    */
+    @Autowired
+    /* Явно указываем имя внедряемого bean-a */
+    @Qualifier("poolStarter")
+    private StarterConnectionPool starterConnectionPool;
+
+    /* Для просмотра */
+    public StarterConnectionPool getStarterConnectionPool() {
+        return starterConnectionPool;
+    }
+}
