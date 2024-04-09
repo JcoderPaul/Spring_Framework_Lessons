7d6973dbc9080a7b5ba63d1161e4f222e3213eb6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-09 16:59:07 +0500

    Add Auditing.java

---
+++ Spring_part_3/src/main/java/spring/oldboy/bean_post_processor/Auditing.java
@@ -0,0 +1,11 @@
+package spring.oldboy.bean_post_processor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Auditing {
+}
