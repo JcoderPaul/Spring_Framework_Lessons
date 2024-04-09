2c0c5e21d15eb039e26081539661b6e7a465c4b1
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-09 16:59:38 +0500

    Add InjectBean.java

---
+++ Spring_part_3/src/main/java/spring/oldboy/bean_post_processor/InjectBean.java
@@ -0,0 +1,29 @@
+package spring.oldboy.bean_post_processor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+Поскольку наша аннотация нужна будет в RunTime, то RetentionPolicy.RUNTIME,
+т.к. по умолчанию RetentionPolicy.CLASS. Аннотация @Target указывает над чем
+будет использоваться наша аннотация - над полем (FIELD).
+*/
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface InjectBean {
+    /*
+    Нам понадобится bean пост-процессор, который будет смотреть аннотации над
+    полями bean-ов и если такое поле есть, то он будет искать в контексте
+    требуемый bean и внедрять его.
+
+    Т.е. если мы аннотируем данной аннотацией поле:
+
+    @InjectBean
+    private StarterConnectionPool starterConnectionPool;
+
+    То наш самописный пост-процессор найдет bean класса StarterConnectionPool и
+    сделает инъекцию в данное поле.
+    */
+}
