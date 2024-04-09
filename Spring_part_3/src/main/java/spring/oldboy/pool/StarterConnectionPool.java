145a072180b13b15fd0f6d8a8f7466edc488c3ec
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-09 16:55:42 +0500

    Edit StarterConnectionPool.java

--- Spring_part_3/src/main/java/spring/oldboy/pool/StarterConnectionPool.java
+++ Spring_part_3/src/main/java/spring/oldboy/pool/StarterConnectionPool.java
@@ -1,5 +1,7 @@
 package spring.oldboy.pool;
+
 /* Примерный вид bean-a из реального приложения - неизменяем, много аннотаций */
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
