72a5cd549fec4c2c63c771630155b0f277b12f43
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-09 18:41:52 +0500

    Add LogBeanFactoryPostProcessor.java

---
+++ Spring_part_4/src/main/java/spring/oldboy/bean_factory_pp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,26 @@
+package spring.oldboy.bean_factory_pp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws
+                                                                                 BeansException {
+        System.out.println("1.postProcessBeanFactory - LogBeanFactoryPostProcessor");
+    }
+    /*
+    Влияем на порядок инициализации (запуска) порядок
+    запуска этих экземпляров BeanFactoryPostProcessor bean-ов
+    */
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
