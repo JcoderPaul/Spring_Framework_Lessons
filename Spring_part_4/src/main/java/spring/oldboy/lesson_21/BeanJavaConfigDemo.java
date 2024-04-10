1913d5f94a78c6aca7336cb47d530a7d32a07921
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-10 14:03:13 +0500

    Edit BeanJavaConfigDemo.java

--- Spring_part_4/src/main/java/spring/oldboy/lesson_21/BeanJavaConfigDemo.java
+++ Spring_part_4/src/main/java/spring/oldboy/lesson_21/BeanJavaConfigDemo.java
@@ -7,7 +7,7 @@ import spring.oldboy.repository.UserRepository;
 public class BeanJavaConfigDemo {
     public static void main(String[] args) {
 
-        /* Получаем контекст из *.xml */
+        /* Получаем контекст из нашего ApplicationConfiguration */
         try(AnnotationConfigApplicationContext myContext =
                     new AnnotationConfigApplicationContext(ApplicationConfiguration.class)){
 
