539734ed7bc15635a124c2bd3140cbf013ce74b0
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-10 18:02:46 +0500

    Edit AccessType.java

--- Spring_part_5/src/main/java/spring/oldboy/listener/entity/AccessType.java
+++ Spring_part_5/src/main/java/spring/oldboy/listener/entity/AccessType.java
@@ -1,5 +1,7 @@
 package spring.oldboy.listener.entity;
 
+/* Возможные события с некой сущностью, которе нужно зафиксировать (прослушать) */
+
 public enum AccessType {
     CREATE, UPDATE, READ, DELETE
 }
