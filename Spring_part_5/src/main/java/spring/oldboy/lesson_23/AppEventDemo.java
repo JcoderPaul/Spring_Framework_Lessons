07c0dc0e5b5cad5a06a0039626371110de8653af
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-10 15:53:07 +0500

    Add AppEventDemo.java

---
+++ Spring_part_5/src/main/java/spring/oldboy/lesson_23/AppEventDemo.java
@@ -0,0 +1,24 @@
+package spring.oldboy.lesson_23;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.oldboy.config.ApplicationConfiguration;
+import spring.oldboy.pool.ConnectionPool;
+import spring.oldboy.service.CompanyService;
+
+public class AppEventDemo {
+    public static void main(String[] args) {
+
+        try (var context = new AnnotationConfigApplicationContext()) {
+            context.register(ApplicationConfiguration.class);
+            context.refresh();
+
+            ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
+            System.out.println(connectionPool);
+
+            CompanyService companyService = context.getBean(CompanyService.class);
+            System.out.println(companyService.findById(1));
+            System.out.println("\n----------------------------------------------------\n");
+            System.out.println(context.getApplicationListeners());
+        }
+    }
+}
