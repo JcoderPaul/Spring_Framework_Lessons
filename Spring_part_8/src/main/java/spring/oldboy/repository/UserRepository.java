e5292e659716c9b8d7460dd481814428a75a0b71
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-12 14:34:16 +0500

    Add UserRepository.java

---
+++ Spring_part_8/src/main/java/spring/oldboy/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package spring.oldboy.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+import spring.oldboy.pool.ConnectionPool;
+
+@Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@RequiredArgsConstructor
+public class UserRepository {
+    /* Перенос данной аннотации в конструктор прописан в lombok.config */
+    @Qualifier("pool2")
+    private final ConnectionPool connectionPool;
+
+}
