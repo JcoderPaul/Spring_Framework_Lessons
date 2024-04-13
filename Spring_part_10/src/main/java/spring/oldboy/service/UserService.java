dced528da68cd901a3cd62f102a3c0e4548befb1
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-13 20:12:31 +0500

    Add UserService.java

---
+++ Spring_part_10/src/main/java/spring/oldboy/service/UserService.java
@@ -0,0 +1,21 @@
+package spring.oldboy.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.oldboy.database.repository.company_repository.CompanyRepository;
+import spring.oldboy.database.repository.user_repository.UserRepository;
+
+/*
+Используем Lombok аннотации для создания конструктора
+с final полями. Если бы у нас были не final поля, то
+мы воспользовались бы @AllArgsConstructor
+*/
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
+
+}
