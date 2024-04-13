533f8c42739460c2e2411fdc0275526226f86cb6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-13 14:30:46 +0500

    Add CompanyRepository.java

---
+++ Spring_part_9/src/main/java/spring/oldboy/database/repository/CompanyRepository.java
@@ -0,0 +1,46 @@
+package spring.oldboy.database.repository;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+import spring.oldboy.database.entity.Company;
+import spring.oldboy.database.pool.ConnectionPool;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/* Подключаем логер */
+@Slf4j
+@Repository
+@Scope(BeanDefinition.SCOPE_SINGLETON)
+@RequiredArgsConstructor
+public class CompanyRepository implements CrudRepository<Integer, Company> {
+
+    private final ConnectionPool pool1;
+    private final List<ConnectionPool> pools;
+    /* Перенос данной аннотации в конструктор прописан в lombok.config */
+    @Value("${db.pool.size}")
+    private final Integer poolSize;
+
+    @PostConstruct
+    /* Логируем данные */
+    private void init() {
+        log.warn("init company repository");
+    }
+
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("findById method...");
+        return Optional.of(new Company(id, null, Collections.emptyMap()));
+    }
+
+    @Override
+    public void delete(Company entity) {
+        System.out.println("delete method...");
+    }
+}
