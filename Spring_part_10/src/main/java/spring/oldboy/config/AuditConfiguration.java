e7db39fef0b85396dc09c48ad4bd5c7f82b7098c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-13 20:07:03 +0500

    Add AuditConfiguration.java

---
+++ Spring_part_10/src/main/java/spring/oldboy/config/AuditConfiguration.java
@@ -0,0 +1,46 @@
+package spring.oldboy.config;
+
+/*
+Lesson 58 - Создаем конфигурацию для нашего аудита
+Lesson 59 - Аннотируем нашу сущность занимающуюся аудитом как  @EnableEnversRepositories
+*/
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.envers.repository.config.EnableEnversRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import spring.oldboy.SpringAppRunner;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+/*
+Подключаем механизм Hibernate Envers, а так же получаем доступ параметрам таблиц
+аудируемых сущностей. В параметрах передадим наш базовый пакет: spring.oldboy.
+В противном случае сканированию подвергнется только: spring/oldboy/config/condition.
+См. DOC/DataEnvers/EnableEnversRepositories.txt
+*/
+@EnableEnversRepositories(basePackageClasses = SpringAppRunner.class)
+@Configuration
+public class AuditConfiguration {
+
+    /*
+    Для того чтобы передать данные в наши аудирующие поля
+    нам нужен провайдер, в нашем случае передающий String,
+    хотя туда могут улететь и ID и Email и т.д.
+    */
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        /*
+        В реальных приложениях мы в поля определяющие того, кто создал
+        сущность или изменил ее, обычно, передаем данные из службы
+        безопасности - SecurityContext.getCurrentUser().getEmail() или
+        SecurityContext.getCurrentUser().getId(), но в нашем случае мы
+        пока тренируемся и просто передаем объект String.
+
+        AuditorAware функциональный интерфейс, передадим хардкодный текст.
+        */
+        return () -> Optional.of("oldboy");
+    }
+}
