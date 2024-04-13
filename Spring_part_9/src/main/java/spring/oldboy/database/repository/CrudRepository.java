431277bef4211d5855fb648f65e1ce346d8b4b4f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-13 14:30:59 +0500

    Add CrudRepository.java

---
+++ Spring_part_9/src/main/java/spring/oldboy/database/repository/CrudRepository.java
@@ -0,0 +1,10 @@
+package spring.oldboy.database.repository;
+
+import java.util.Optional;
+
+public interface CrudRepository<K, E> {
+
+    Optional<E> findById(K id);
+
+    void delete(E entity);
+}
