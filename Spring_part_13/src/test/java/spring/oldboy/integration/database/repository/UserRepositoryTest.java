26e0e6e47b9da8aa8f847ea9daa8975d53b14259
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-15 20:56:55 +0500

    Part 13: Add UserRepositoryTest.java

---
+++ Spring_part_13/src/test/java/spring/oldboy/integration/database/repository/UserRepositoryTest.java
@@ -0,0 +1,117 @@
+package spring.oldboy.integration.database.repository;
+/* Lesson 62 - тестирование приложения с контейнером Docker PostgreSQL */
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import spring.oldboy.database.entity.Role;
+import spring.oldboy.database.entity.User;
+import spring.oldboy.database.repository.user_repository.UserRepository;
+import spring.oldboy.dto.PersonalInfo;
+import spring.oldboy.dto.PersonalInfoTwo;
+import spring.oldboy.dto.UserFilterDto;
+import spring.oldboy.integration.IntegrationTestBase;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RequiredArgsConstructor
+class UserRepositoryTest extends IntegrationTestBase {
+
+    private final UserRepository userRepository;
+
+    @Test
+    void checkBatch() {
+        List<User> users = userRepository.findAll();
+        userRepository.updateCompanyAndRole(users);
+        System.out.println();
+    }
+
+    @Test
+    void checkJdbcTemplate() {
+        List<PersonalInfo> users = userRepository.findAllByCompanyIdAndRole(1, Role.USER);
+        assertThat(users).hasSize(1);
+    }
+
+    @Test
+    void checkAuditing() {
+        User ivan = userRepository.findById(1L).get();
+        ivan.setBirthDate(ivan.getBirthDate().plusYears(1L));
+        userRepository.flush();
+        System.out.println();
+    }
+
+    @Test
+    void checkCustomImplementation() {
+        UserFilterDto filter = new UserFilterDto(null,
+                                                 "%ov%",
+                                                 LocalDate.now());
+        List<User> users = userRepository.findAllByFilter(filter);
+        assertThat(users).hasSize(4);
+    }
+
+    @Test
+    void checkProjections() {
+        List<PersonalInfoTwo> users = userRepository.findAllByCompanyId(1);
+        assertThat(users).hasSize(2);
+    }
+
+    @Test
+    void checkPageable() {
+        PageRequest pageable = PageRequest.of(0, 2, Sort.by("id"));
+        Page<User> slice = userRepository.findAllBy(pageable);
+        slice.forEach(user -> System.out.println(user.getCompany().getName()));
+
+        while (slice.hasNext()) {
+            slice = userRepository.findAllBy(slice.nextPageable());
+            slice.forEach(user -> System.out.println(user.getCompany().getName()));
+        }
+    }
+
+    @Test
+    void checkSort() {
+        Sort.TypedSort<User> sortBy = Sort.sort(User.class);
+        Sort sort = sortBy.by(User::getFirstname)
+                .and(sortBy.by(User::getLastname));
+
+        Sort sortById = Sort.by("firstname").and(Sort.by("lastname"));
+        List<User> allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkFirstTop() {
+        Optional<User> topUser = userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent(user -> assertEquals(5L, user.getId()));
+    }
+
+    @Test
+    void checkUpdate() {
+        User ivan = userRepository.getReferenceById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+        ivan.setBirthDate(LocalDate.now());
+
+        int resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+        ivan.getCompany().getName();
+
+        User theSameIvan = userRepository.getReferenceById(1L);
+        assertSame(Role.USER, theSameIvan.getRole());
+    }
+
+    @Test
+    void checkQueries() {
+        List<User> users = userRepository.findAllBy("a", "ov");
+        assertThat(users).hasSize(3);
+    }
+}
