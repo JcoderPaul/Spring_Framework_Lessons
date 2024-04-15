36bdbfa039fe7fc12ab466f52ec2781e692ff4e8
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-15 20:39:04 +0500

    Part 13: Add TestApplicationRunner.java

---
+++ Spring_part_13/src/test/java/spring/oldboy/integration/TestApplicationRunner.java
@@ -0,0 +1,13 @@
+package spring.oldboy.integration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import spring.oldboy.database.pool.ConnectionPool;
+
+@TestConfiguration
+public class TestApplicationRunner {
+
+    @SpyBean(name = "pool1")
+    private ConnectionPool pool1;
+
+}
