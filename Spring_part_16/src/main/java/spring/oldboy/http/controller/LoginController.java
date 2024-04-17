96f053f34ff8039c527eb9ee425c4ba6a2a0f657
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-17 12:32:38 +0500

    Part 16: Add LoginController.java

---
+++ Spring_part_16/src/main/java/spring/oldboy/http/controller/LoginController.java
@@ -0,0 +1,30 @@
+package spring.oldboy.http.controller;
+
+/* Part 15: Lesson 75 - Пример передачи параметров запроса в его теле */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring.oldboy.dto.LoginDto;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "user/login";
+    }
+
+    /*
+    Part 15: Lesson 75:
+    Аннотацией @ModelAttribute("login") задаем имя ключа параметра, сами
+    параметры при отправке формы логина будут инжектированы в loginDto.
+    */
+    @PostMapping("/login")
+    public String login(Model model, @ModelAttribute("login") LoginDto loginDto) {
+        return "user/login";
+    }
+
+}
