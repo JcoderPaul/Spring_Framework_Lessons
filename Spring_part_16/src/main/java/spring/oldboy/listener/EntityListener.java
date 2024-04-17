c2016fb12d2537c8c68207c5a7241a04c42e1d7f
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-17 12:14:06 +0500

    Part 16: Add EntityListener.java

---
+++ Spring_part_16/src/main/java/spring/oldboy/listener/EntityListener.java
@@ -0,0 +1,51 @@
+package spring.oldboy.listener;
+
+/*
+Наш слушатель событий.
+
+И снова про паттерн 'слушатель':
+- нам нужен Event (у нас это EntityEvent.java), некий генератор событий;
+- нам нужен Listener (у нас это EntityListener.java), слушатель и обработчик событий;
+*/
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/*
+Поскольку он Sprig сущность (компонент, bean) аннотируем его,
+для того чтобы он находился в Spring контексте и мог прослушивать
+требуемые события
+*/
+@Component
+public class EntityListener {
+
+    /*
+    Метод доступа к прослушиваемой Entity, куда она передается
+    в качестве параметра. Аннотируем наш метод, придавая ему
+    функциональность слушателя.
+
+    Spring неявно на основании каждой аннотации @EventListener
+    создаст объект типа ApplicationListener.
+    */
+    @EventListener
+    public void acceptEntity(EntityEvent entityEvent) {
+        /*
+        Тут реализуется необходимая логика: аудит сущности,
+        денормализация БД... т.е. наша бизнес логика
+        */
+        System.out.println("Entity to Listener: " + entityEvent);
+    }
+
+    /*
+    Интересная опция или поле в аннотации @EventListener это 'condition' - условия,
+    что позволяет писать или конфигурировать строго определенные слушатели, например
+    только для реакции на событие чтения (записи, удаления и т.д.).
+
+    @EventListener(condition = "#root.args[0].accessType.name() == 'READ'")
+    @Order(10)
+    public void acceptEntityRead(EntityEvent entityEvent) {
+        System.out.println("Entity: " + entityEvent);
+    }
+
+    */
+}
