d18120150c44e59add9ac5f7f9ee99832323b9c7
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-18 14:17:44 +0500

    Part 17: Add Mapper.java

---
+++ Spring_part_17/src/main/java/spring/oldboy/mapper/Mapper.java
@@ -0,0 +1,16 @@
+package spring.oldboy.mapper;
+/* Обычно F - From (из этого), T - To (в этот) */
+public interface Mapper<F, T> {
+
+    T map(F object);
+    /*
+    Чуть перегрузим метод, фактически он понадобится в тот момент когда разработчику
+    реально понадобится преобразовать объект F в объект T именно таким образом, например
+    методом копирования, реализация метода произойдет именно в том классе, где это нужно.
+    У нас это UserCreateEditMapper.java
+    */
+    default T map(F fromObject, T toObject) {
+        return toObject;
+    }
+
+}
