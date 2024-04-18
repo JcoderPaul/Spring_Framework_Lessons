cf86e978a08c8fc2eb10176d20efbce6c30af20a
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-18 14:09:02 +0500

    Part 17: Add UserCreateEditDto.java

---
+++ Spring_part_17/src/main/java/spring/oldboy/dto/UserCreateEditDto.java
@@ -0,0 +1,26 @@
+package spring.oldboy.dto;
+
+import lombok.Value;
+import lombok.experimental.FieldNameConstants;
+import org.springframework.format.annotation.DateTimeFormat;
+import spring.oldboy.database.entity.Role;
+
+import java.time.LocalDate;
+
+@Value
+/*
+Немного упростим себе жизнь на этапе тестирования. Данная аннотация создает внутренний тип,
+содержащий строковые константы, содержащие имя поля для каждого поля. В качестве альтернативы
+генерирует внутреннее перечисление со значениями перечисления, соответствующими каждому имени
+поля.
+*/
+@FieldNameConstants
+public class UserCreateEditDto {
+    String username;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    LocalDate birthDate;
+    String firstname;
+    String lastname;
+    Role role;
+    Integer companyId;
+}
