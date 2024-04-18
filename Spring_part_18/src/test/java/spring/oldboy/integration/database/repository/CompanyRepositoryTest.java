5eae1ca4f91ce6e7d46b5cd4c695dc54bd8ef660
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-18 20:17:55 +0500

    Part 18: Add CompanyRepositoryTest.java

---
+++ Spring_part_18/src/test/java/spring/oldboy/integration/database/repository/CompanyRepositoryTest.java
@@ -0,0 +1,63 @@
+package spring.oldboy.integration.database.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.transaction.support.TransactionTemplate;
+import spring.oldboy.database.entity.Company;
+import spring.oldboy.database.repository.company_repository.CompanyRepository;
+import spring.oldboy.integration.IntegrationTestBase;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RequiredArgsConstructor
+class CompanyRepositoryTest extends IntegrationTestBase {
+    private static final Integer APPLE_ID = 4;
+    private final EntityManager entityManager;
+    private final TransactionTemplate transactionTemplate;
+    private final CompanyRepository companyRepository;
+
+    @Test
+    void checkFindByQueries() {
+        companyRepository.findByName("google");
+        companyRepository.findAllByNameContainingIgnoreCase("a");
+    }
+
+    @Test
+    void findById() {
+        transactionTemplate.executeWithoutResult(tx -> {
+            Company company = entityManager.find(Company.class, 1);
+            assertNotNull(company);
+            assertThat(company.getLocales()).hasSize(2);
+        });
+    }
+
+    @Test
+    void save() {
+        Company company = Company.builder()
+                .name("Apple1")
+                .locales(Map.of(
+                        "ru", "Apple описание",
+                        "en", "Apple description"
+                ))
+                .build();
+        entityManager.persist(company);
+        assertNotNull(company.getId());
+    }
+
+    @Test
+    @Disabled
+    void delete() {
+        Optional<Company> maybeCompany = companyRepository.findById(APPLE_ID);
+        assertTrue(maybeCompany.isPresent());
+        maybeCompany.ifPresent(companyRepository::delete);
+        entityManager.flush();
+        assertTrue(companyRepository.findById(APPLE_ID).isEmpty());
+    }
+}
