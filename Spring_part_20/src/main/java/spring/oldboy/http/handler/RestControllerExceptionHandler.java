5e15c0d17335ebd38b933af905bfc56ad2234098
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-19 19:44:37 +0500

    Part 20: Add RestControllerExceptionHandler.java

---
+++ Spring_part_20/src/main/java/spring/oldboy/http/handler/RestControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package spring.oldboy.http.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice(basePackages = "spring.oldboy.http.rest")
+public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
+}
