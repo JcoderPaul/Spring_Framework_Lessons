5de51c9b02f36c2125afa3d247f2d1e74aedd7ce
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-20 22:09:22 +0500

    Part 21: Add UserReadDto.java

---
+++ Spring_part_21/src/main/java/spring/oldboy/dto/UserReadDto.java
@@ -0,0 +1,18 @@
+package spring.oldboy.dto;
+
+import lombok.Value;
+import spring.oldboy.database.entity.Role;
+
+import java.time.LocalDate;
+
+@Value
+public class UserReadDto {
+    Long id;
+    String username;
+    LocalDate birthDate;
+    String firstname;
+    String lastname;
+    String image;
+    Role role;
+    CompanyReadDto company;
+}
