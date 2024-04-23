5381f3f0a343f092fa4f3ba55880e65b1e5cd00c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2024-04-23 18:02:54 +0500

    Part 25: Add MessageRestController.java

---
+++ Spring_part_25/src/main/java/spring/oldboy/http/rest/MessageRestController.java
@@ -0,0 +1,37 @@
+package spring.oldboy.http.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+/* REST контроллер */
+@RestController
+/* Стандартная аннотация для картирования */
+@RequestMapping("/api/v1/messages")
+/* Аннотация для конструкторов */
+@RequiredArgsConstructor
+public class MessageRestController {
+
+    /* Это зависимость есть у любого Spring приложения т.к. принадлежит Spring Core */
+    private final MessageSource messageSource;
+
+    /*
+    Метод принимает два параметра которые мы должны передать в ResourceBundle,
+    ну или как мы уже знаем в MessageSource. Внедряем (inject) MessageSource
+    выше по коду и используем тут.
+
+    Ключами или key у нас являются login.username, login.password из файлов
+    messages.properties,
+    */
+    @GetMapping
+    public String getMessage(@RequestParam("key") String key,
+                             @RequestParam("lang") String language) {
+        /* Чтобы метод не падал с исключением мы передаем в него null-вые аргументы */
+        return messageSource.getMessage(key, null, new Locale(language));
+    }
+}
